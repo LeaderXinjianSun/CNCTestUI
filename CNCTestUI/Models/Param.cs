849527ae375a2739a9ea4861ff6b13208dc58e41
sunxinjian <715054307@qq.com>
2022-05-07 18:24:45 +0800

    1

---
+++ CNCTestUI/Models/Param.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CNCTestUI.Models
+{
+    public class Param
+    {
+        public string SerialCOM;
+
+        public double X1Abs;
+        public double Y1Abs;
+        public double Z1Abs;
+        public double R1Abs;
+
+        public double X1JogSpeed;
+        public double Y1JogSpeed;
+        public double Z1JogSpeed;
+        public double R1JogSpeed;
+
+        public double X1RunSpeed;
+        public double Y1RunSpeed;
+        public double Z1RunSpeed;
+        public double R1RunSpeed;
+    }
+}
--- CNCTestUI/ViewModels/MainWindowViewModel.cs
+++ CNCTestUI/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using CNCTestUI.Models;
+using Newtonsoft.Json;
 using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Regions;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +18,8 @@ namespace CNCTestUI.ViewModels
     {
         #region 变量
         AxisParm axisParm = default;
+        Param myParam;
+        string serialCOM = "COM7";
         #endregion
         #region 属性绑定
         private string title = "CNCTestUI";
@@ -126,6 +130,79 @@ namespace CNCTestUI.ViewModels
             get { return motionJogSelectedIndex; }
             set { SetProperty(ref motionJogSelectedIndex, value); }
         }
+        private double x1Abs;
+        public double X1Abs
+        {
+            get { return x1Abs; }
+            set { SetProperty(ref x1Abs, value); }
+        }
+        private double y1Abs;
+        public double Y1Abs
+        {
+            get { return y1Abs; }
+            set { SetProperty(ref y1Abs, value); }
+        }
+        private double z1Abs;
+        public double Z1Abs
+        {
+            get { return z1Abs; }
+            set { SetProperty(ref z1Abs, value); }
+        }
+        private double r1Abs;
+        public double R1Abs
+        {
+            get { return r1Abs; }
+            set { SetProperty(ref r1Abs, value); }
+        }
+        private double x1RunSpeed;
+        public double X1RunSpeed
+        {
+            get { return x1RunSpeed; }
+            set { SetProperty(ref x1RunSpeed, value); }
+        }
+        private double y1RunSpeed;
+        public double Y1RunSpeed
+        {
+            get { return y1RunSpeed; }
+            set { SetProperty(ref y1RunSpeed, value); }
+        }
+        private double z1RunSpeed;
+        public double Z1RunSpeed
+        {
+            get { return z1RunSpeed; }
+            set { SetProperty(ref z1RunSpeed, value); }
+        }
+        private double r1RunSpeed;
+        public double R1RunSpeed
+        {
+            get { return r1RunSpeed; }
+            set { SetProperty(ref r1RunSpeed, value); }
+        }
+
+        private double x1JogSpeed;
+        public double X1JogSpeed
+        {
+            get { return x1JogSpeed; }
+            set { SetProperty(ref x1JogSpeed, value); }
+        }
+        private double y1JogSpeed;
+        public double Y1JogSpeed
+        {
+            get { return y1JogSpeed; }
+            set { SetProperty(ref y1JogSpeed, value); }
+        }
+        private double z1JogSpeed;
+        public double Z1JogSpeed
+        {
+            get { return z1JogSpeed; }
+            set { SetProperty(ref z1JogSpeed, value); }
+        }
+        private double r1JogSpeed;
+        public double R1JogSpeed
+        {
+            get { return r1JogSpeed; }
+            set { SetProperty(ref r1JogSpeed, value); }
+        }
         #endregion
         #region 方法绑定
         private DelegateCommand<object> menuCommand;
@@ -156,22 +233,75 @@ namespace CNCTestUI.ViewModels
         private DelegateCommand<object> jogN_MouseDownCommand;
         public DelegateCommand<object> JogN_MouseDownCommand =>
             jogN_MouseDownCommand ?? (jogN_MouseDownCommand = new DelegateCommand<object>(ExecuteJogN_MouseDownCommand));
+        private DelegateCommand<object> getAbsCommand;
+        public DelegateCommand<object> GetAbsCommand =>
+            getAbsCommand ?? (getAbsCommand = new DelegateCommand<object>(ExecuteGetAbsCommand));
+        private DelegateCommand saveParamCommand;
+        public DelegateCommand SaveParamCommand =>
+            saveParamCommand ?? (saveParamCommand = new DelegateCommand(ExecuteSaveParamCommand));
 
+        void ExecuteSaveParamCommand()
+        {
+            myParam.X1RunSpeed = X1RunSpeed;
+            myParam.Y1RunSpeed = Y1RunSpeed;
+            myParam.Z1RunSpeed = Z1RunSpeed;
+            myParam.R1RunSpeed = R1RunSpeed;
+
+            myParam.X1JogSpeed = X1JogSpeed;
+            myParam.Y1JogSpeed = Y1JogSpeed;
+            myParam.Z1JogSpeed = Z1JogSpeed;
+            myParam.R1JogSpeed = R1JogSpeed;
+
+            string jsonString = JsonConvert.SerializeObject(myParam, Formatting.Indented);
+            File.WriteAllText(System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "Param.json"), jsonString);
+            MessageBox.Show("参数保存完成", "信息", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        void ExecuteGetAbsCommand(object obj)
+        {
+            switch (obj.ToString())
+            {
+                case "0":
+                    X1Abs = myParam.X1Abs = ServoModbus.Instance.ReadInovance(1) * GTSCard.Instance.X1.Equiv;
+                    GTSCard.Instance.SigAxisPosSet(GTSCard.Instance.X1, 0);
+                    GTSCard.Instance.SigAxisEncSet(GTSCard.Instance.X1, 0);
+                    break;
+                case "1":
+                    Y1Abs = myParam.Y1Abs = ServoModbus.Instance.ReadInovance(2) * GTSCard.Instance.Y1.Equiv;
+                    GTSCard.Instance.SigAxisPosSet(GTSCard.Instance.Y1, 0);
+                    GTSCard.Instance.SigAxisEncSet(GTSCard.Instance.Y1, 0);
+                    break;
+                case "2":
+                    Z1Abs = myParam.Z1Abs = ServoModbus.Instance.ReadInovance(3) * GTSCard.Instance.Z1.Equiv;
+                    GTSCard.Instance.SigAxisPosSet(GTSCard.Instance.Z1, 0);
+                    GTSCard.Instance.SigAxisEncSet(GTSCard.Instance.Z1, 0);
+                    break;
+                case "3":
+                    R1Abs = myParam.R1Abs = ServoModbus.Instance.ReadInovance(4) * GTSCard.Instance.R1.Equiv;
+                    GTSCard.Instance.SigAxisPosSet(GTSCard.Instance.R1, 0);
+                    GTSCard.Instance.SigAxisEncSet(GTSCard.Instance.R1, 0);
+                    break;
+                default:
+                    break;
+            }
+            string jsonString = JsonConvert.SerializeObject(myParam, Formatting.Indented);
+            File.WriteAllText(System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "Param.json"), jsonString);
+            MessageBox.Show("零点设置完成", "信息", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
         void ExecuteJogN_MouseDownCommand(object obj)
         {
             switch (obj.ToString())
             {
                 case "0":
-                    GTSCard.Instance.AxisJog(GTSCard.Instance.X1, 0, 50);
+                    GTSCard.Instance.AxisJog(GTSCard.Instance.X1, 0, X1JogSpeed);
                     break;
                 case "1":
-                    GTSCard.Instance.AxisJog(GTSCard.Instance.Y1, 0, 50);
+                    GTSCard.Instance.AxisJog(GTSCard.Instance.Y1, 0, Y1JogSpeed);
                     break;
                 case "2":
-                    GTSCard.Instance.AxisJog(GTSCard.Instance.Z1, 0, 50);
+                    GTSCard.Instance.AxisJog(GTSCard.Instance.Z1, 0, Z1JogSpeed);
                     break;
                 case "4":
-                    GTSCard.Instance.AxisJog(GTSCard.Instance.R1, 0, 50);
+                    GTSCard.Instance.AxisJog(GTSCard.Instance.R1, 0, R1JogSpeed);
                     break;
                 default:
                     break;
@@ -182,16 +312,16 @@ namespace CNCTestUI.ViewModels
             switch (obj.ToString())
             {
                 case "0":
-                    GTSCard.Instance.AxisJog(GTSCard.Instance.X1, 1, 50);
+                    GTSCard.Instance.AxisJog(GTSCard.Instance.X1, 1, X1JogSpeed);
                     break;
                 case "1":
-                    GTSCard.Instance.AxisJog(GTSCard.Instance.Y1, 1, 50);
+                    GTSCard.Instance.AxisJog(GTSCard.Instance.Y1, 1, Y1JogSpeed);
                     break;
                 case "2":
-                    GTSCard.Instance.AxisJog(GTSCard.Instance.Z1, 1, 50);
+                    GTSCard.Instance.AxisJog(GTSCard.Instance.Z1, 1, Z1JogSpeed);
                     break;
                 case "4":
-                    GTSCard.Instance.AxisJog(GTSCard.Instance.R1, 1, 50);
+                    GTSCard.Instance.AxisJog(GTSCard.Instance.R1, 1, R1JogSpeed);
                     break;
                 default:
                     break;
@@ -307,7 +437,31 @@ namespace CNCTestUI.ViewModels
             MotionJogSelectedIndex = 0;
             axisParm = GTSCard.Instance.X1;
             GTSCard.Instance.Init();
-            ServoModbus.Instance.Connect("COM7");
+            LoadParm();
+            if (ServoModbus.Instance.Connect(serialCOM))
+            {
+                var a = ServoModbus.Instance.ReadInovance(1) * GTSCard.Instance.X1.Equiv;
+                GTSCard.Instance.SigAxisPosSet(GTSCard.Instance.X1, a - myParam.X1Abs);
+                GTSCard.Instance.SigAxisEncSet(GTSCard.Instance.X1, a - myParam.X1Abs);
+
+                a = ServoModbus.Instance.ReadInovance(2) * GTSCard.Instance.Y1.Equiv;
+                GTSCard.Instance.SigAxisPosSet(GTSCard.Instance.Y1, a - myParam.Y1Abs);
+                GTSCard.Instance.SigAxisEncSet(GTSCard.Instance.Y1, a - myParam.Y1Abs);
+
+                a = ServoModbus.Instance.ReadInovance(3) * GTSCard.Instance.Z1.Equiv;
+                GTSCard.Instance.SigAxisPosSet(GTSCard.Instance.Z1, a - myParam.Z1Abs);
+                GTSCard.Instance.SigAxisEncSet(GTSCard.Instance.Z1, a - myParam.Z1Abs);
+
+                a = ServoModbus.Instance.ReadInovance(4) * GTSCard.Instance.R1.Equiv;
+                GTSCard.Instance.SigAxisPosSet(GTSCard.Instance.R1, a - myParam.R1Abs);
+                GTSCard.Instance.SigAxisEncSet(GTSCard.Instance.R1, a - myParam.R1Abs);
+            }
+            else
+            {
+                addMessage("串口打开失败");
+            }
+            
+
             UIRun();
         }
         #endregion
@@ -330,10 +484,10 @@ namespace CNCTestUI.ViewModels
                 {
                     if (GTSCard.Instance.OpenCardOk)
                     {
-                        X_Enc = (double)ServoModbus.Instance.ReadInovance(1) / 1000;
-                        Y_Enc = (double)ServoModbus.Instance.ReadInovance(2) / 1000;
-                        Z_Enc = (double)ServoModbus.Instance.ReadInovance(3) / 1000;
-                        R_Enc = (double)ServoModbus.Instance.ReadInovance(4) / 1000;
+                        X_Enc = GTSCard.Instance.GetEnc(GTSCard.Instance.X1);
+                        Y_Enc = GTSCard.Instance.GetEnc(GTSCard.Instance.Y1);
+                        Z_Enc = GTSCard.Instance.GetEnc(GTSCard.Instance.Z1);
+                        R_Enc = GTSCard.Instance.GetEnc(GTSCard.Instance.R1);
                         if (AxisViewVisibility == "Visible")
                         {
                             AxisStatus axisStatus = GTSCard.Instance.GetAxisStatus(axisParm);
@@ -363,6 +517,38 @@ namespace CNCTestUI.ViewModels
                 MessageStr += "\n";
             }
             MessageStr += str;
+        }
+        private void LoadParm()
+        {
+            try
+            {
+                //Json序列化，从文件读取
+                string jsonString = File.ReadAllText(System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "Param.json"));
+                myParam = JsonConvert.DeserializeObject<Param>(jsonString);
+
+                serialCOM = myParam.SerialCOM;
+
+                X1Abs = myParam.X1Abs;
+                Y1Abs = myParam.Y1Abs;
+                Z1Abs = myParam.Z1Abs;
+                R1Abs = myParam.R1Abs;
+
+                X1RunSpeed = myParam.X1RunSpeed;
+                Y1RunSpeed = myParam.Y1RunSpeed;
+                Z1RunSpeed = myParam.Z1RunSpeed;
+                R1RunSpeed = myParam.R1RunSpeed;
+
+                X1JogSpeed = myParam.X1JogSpeed;
+                Y1JogSpeed = myParam.Y1JogSpeed;
+                Z1JogSpeed = myParam.Z1JogSpeed;
+                R1JogSpeed = myParam.R1JogSpeed;
+
+
+            }
+            catch (Exception ex)
+            {
+                addMessage(ex.Message);
+            }
         }
         #endregion
     }
